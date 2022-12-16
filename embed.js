4d8be9e4d89da7d132245b956e45ea8903e6c2db
Spacey_Core <59103833+Exaby@users.noreply.github.com>
2022-12-16 11:44:18 -0500

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,78 @@
+addEventListener('fetch', event => {
+event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+const url = new URL(request.url)
+const embedUrl = url.searchParams.get('url')
+if (!embedUrl) {
+return new Response('Error: No URL provided', {
+status: 400,
+headers: { 'Content-Type': 'text/plain' },
+})
+}
+
+// Send a request to the URL and get the response
+const response = await fetch(embedUrl)
+
+// Get the content type and status of the response
+const contentType = response.headers.get('Content-Type')
+const status = response.status
+const color = "#40006a";
+
+// Check the content type and return an appropriate response
+if (contentType.startsWith('image/')) {
+const html = `
+
+<head>
+  <title>${contentType} | ${status} | ${embedUrl}</title>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex">
+  <meta name="theme-color" content="${color}">
+  <meta property="og:image" content="${embedUrl}">
+  <meta property="twitter:card" content="summary_large_image">
+  <link rel="stylesheet" href="https://cdn.spco.xyz/video/c/TopLevelVideoDocument.css" />
+  <link rel="stylesheet" href="https://cdn.spco.xyz/video/c/BottomLevelVideoDocument.css" />
+</head>
+
+<body>
+  <img src="${embedUrl}">
+</body>
+
+</html>
+`
+return new Response(html, {
+headers: { 'Content-Type': 'text/html' },
+})
+} else if (contentType.startsWith('video/')) {
+const html = `
+
+<head>
+  <title>${contentType} | ${status} | ${embedUrl}</title>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex">
+  <meta name="theme-color" content="${color}">
+  <meta property="og:video" content="${embedUrl}">
+  <meta property="twitter:card" content="summary_large_image">
+  <link rel="stylesheet" href="https://cdn.spco.xyz/video/c/videocontrols.css" />
+  <link rel="stylesheet" href="https://cdn.spco.xyz/video/c/TopLevelVideoDocument.css" />
+  <link rel="stylesheet" href="https://cdn.spco.xyz/video/c/BottomLevelVideoDocument.css" />
+  <script type="text/javascript" src="https://cdn.spco.xyz/video/j/TopLevelVideoDocument.js"></script>
+</head>
+
+<body>
+  <video src="${embedUrl}" autoplay controls>
+</body>
+
+</html>
+`
+return new Response(html, {
+headers: { 'Content-Type': 'text/html' },
+})
+} else {
+return new Response(`Error: Unsupported content type "${contentType}"`, {
+status: 400,
+headers: { 'Content-Type': 'text/plain' },
+})
+}
+}
